1b4ac865ed6de42962841e021bee2b5ced494f3b
Konstantin Pak <konstantin.pak.it@gmail.com>
2023-03-01 11:02:47 +0600

    Форма для записи к доктору завершена. Полностью адаптивна

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,46 @@
 'use strict'
 
 const mobileMenu = document.querySelector('.mobile-menu');
+const chooseServiceSelectBox = document.querySelector('.choose-service-input__select-box'); 
+const chooseServiceDropdown = document.querySelector('.choose-service-input__dropdown');
+const chooseServiceOptions = document.querySelectorAll('.choose-service-input__item');
+const chooseServiceLabel = document.querySelector('.choose-service-input__label');
+const chooseServiceChevronIcon = document.querySelector('.choose-service-input__chevron-down-wrapper');
+
+const chooseServiceCitySelectBox = document.querySelector("[data-city-select]");
+const chooseServiceCityDropdown = document.querySelector('[data-city-dropdown]');
+const chooseServiceCityOptions = document.querySelectorAll('.city-option');
+const chooseServiceCityLabel = document.querySelector('.city-label');
+const chooseServiceCityChevronIcon = document.querySelector('.city-option-chevron');
 
 mobileMenu.addEventListener("click", function() {
   mobileMenu.classList.toggle("active");
 });
+
+chooseServiceSelectBox.addEventListener("click", function() {
+  chooseServiceDropdown.classList.toggle("active");
+  chooseServiceChevronIcon.classList.toggle("active");
+});
+
+chooseServiceCitySelectBox.addEventListener("click", function() {
+  chooseServiceCityDropdown.classList.toggle("active");
+  chooseServiceCityChevronIcon.classList.toogle("active");
+});
+
+chooseServiceOptions.forEach(o => {
+  o.addEventListener("click", function() {
+    chooseServiceLabel.innerHTML = o.querySelector("input").value;
+    chooseServiceDropdown.classList.remove("active");
+    chooseServiceChevronIcon.classList.remove("active");
+  });
+});
+
+chooseServiceCityOptions.forEach(o => {
+  o.addEventListener("click", function() {
+    chooseServiceCityLabel.innerHTML = o.querySelector("input").value;
+    chooseServiceCityDropdown.classList.remove("active");
+    chooseServiceCityChevronIcon.classList.remove("active");
+
+  });
+});
+
